aa5c2087812b18b0658b69b8763b8cf7bf9808c3
matias-cavalcante <matias.cavalcante93@gmail.com>
2023-05-15 17:18:30 +0000

    Website ready to be deployed

--- Script.js
+++ Script.js
@@ -1,11 +1,32 @@
 
+function toggleMenu() {
+    const navbar = document.getElementById("mobile-nav");
+    const body = document.body;
+    
+    if (navbar.classList.contains('active')) {
+        navbar.classList.remove('active');
+        body.classList.remove('disable-scroll');
+    } else {
+        navbar.classList.add('active');
+        body.classList.add('disable-scroll');
+    }
+}
 
-window.addEventListener('scroll', function() {
+document.querySelectorAll('#mobile-nav a').forEach(function(link) {
+    link.addEventListener('click', toggleMenu);
+  });
+  
+  window.addEventListener('scroll', function() {
     var cardSection = document.getElementById('cards');
     var position = cardSection.getBoundingClientRect();
-
     // checking whether fully visible
-    if(position.top >= 0 && position.bottom <= window.innerHeight) {
+    if(position.top < window.innerHeight && position.bottom > 0) {
         cardSection.classList.add('animate');
     }
 });
+
+function navigateToMyProjects() {
+    // perform any action you need here
+    location.href = "#my-projects";
+}
+
